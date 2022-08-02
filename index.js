9f0ed7473154f962a17963bd43af752a9ce20347
vyshnavi12-akf <gvyshnavi2003@gmail.com>
2022-08-02 04:02:48 +0000

    solution

---
+++ index.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+class Hello extends Component {
+  render() {
+    return <h1 className="heading">Hello Vyshu</h1>;
+  }
+ ReactDOM.render(<Hello/>,document.getElementById("root"))
+}
